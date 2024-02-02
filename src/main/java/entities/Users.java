3fb3f6c2c849a45f0890b535b98254db187ccfb9
rahimuj570 <rahimuj570@gmail.com>
2024-02-02 22:17:44 +0600

    user entities added with hibernate anotation

---
+++ src/main/java/entities/Users.java
@@ -0,0 +1,72 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int user_id;
+	private String user_name;
+	private String user_email;
+	private String user_password;
+	private String user_gender;
+
+	public Users(int user_id, String user_name, String user_email, String user_password, String user_gender) {
+		super();
+		this.user_id = user_id;
+		this.user_name = user_name;
+		this.user_email = user_email;
+		this.user_password = user_password;
+		this.user_gender = user_gender;
+	}
+
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	public String getUser_gender() {
+		return user_gender;
+	}
+
+	public void setUser_gender(String user_gender) {
+		this.user_gender = user_gender;
+	}
+
+}
